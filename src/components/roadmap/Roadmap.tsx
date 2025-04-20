b0a84547c866716ff9b40f870b4fff3d80a4dd57
aurilaus <akhor@nordpage.ru>
2025-04-20 10:28:36 +0300

    upd

--- src/components/roadmap/Roadmap.tsx
+++ src/components/roadmap/Roadmap.tsx
@@ -7,22 +7,22 @@ const Roadmap : React.FC = () => {
             <h2>Roadmap Windfall</h2>
             <div className={styles.timeline}>
                 <div className={styles.timelineItem}>
-                    <div className={styles.timelineDate}>Весна 2025</div>
+                    <div className={styles.timelineDate}>Апрель - Май 2025</div>
                     <div className={styles.timelineContent}>Базовая тележка, 3D-модель осла и прототип геймплея
                         (движение,
                         зельеварение, выращивание растений)
                     </div>
                 </div>
                 <div className={styles.timelineItem}>
-                    <div className={styles.timelineDate}>Лето 2025</div>
+                    <div className={styles.timelineDate}>Июнь - Июль 2025</div>
                     <div className={styles.timelineContent}>Визуальные правки, интерфейс, внутриигровой цикл</div>
                 </div>
                 <div className={styles.timelineItem}>
-                    <div className={styles.timelineDate}>Осень 2025</div>
+                    <div className={styles.timelineDate}>Август - Сентябрь 2025</div>
                     <div className={styles.timelineContent}>Графика, сцены, анимации, подготовка первой демки</div>
                 </div>
                 <div className={styles.timelineItem}>
-                    <div className={styles.timelineDate}>Зима 2025</div>
+                    <div className={styles.timelineDate}>Октябрь - Ноябрь 2025</div>
                     <div className={styles.timelineContent}>Подготовка к альфа-тесту</div>
                 </div>
             </div>
